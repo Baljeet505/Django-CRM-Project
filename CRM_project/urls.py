a55cbd954e005b60a2f678454ad2d51886715ccd
Baljeet_Singh <109136064+Baljeet505@users.noreply.github.com>
2023-11-20 10:16:36 +0530

    Update urls.py

--- CRM_project/urls.py
+++ CRM_project/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('CRM_app/',include('CRM_app.urls')),
+    path('',include('CRM_app.urls')),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
